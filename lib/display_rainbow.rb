79e7e02ef6bca2bc546af6ddd8d58bd93593160e
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 17:32:29 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,5 @@
 # Write your #display_rainbow method here
-def display_rainbow(["red", "orange", "yellow", "green", "blue", "violet"])
+colors = ["red", "orange", "yellow", "green", "blue", "violet"]
+def display_rainbow(colors)
   puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]}, B: #{display_rainbow[4]}, V: #{display_rainbow}]
 end
