0dcb3aa52d9c2c47104014efa6deba8ed6529e10
balanced-widget-4002 <jessikim807@gmail.com>
2020-02-01 17:31:36 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,3 @@
 def display_rainbow(["red", "orange", "yellow", "green", "blue", "violet"])
   puts "R: #{display_rainbow[0]}, O: #{display_rainbow[1]}, Y: #{display_rainbow[2]}, G: #{display_rainbow[3]}, B: #{display_rainbow[4]}, V: #{display_rainbow}]
 end
-
-  
